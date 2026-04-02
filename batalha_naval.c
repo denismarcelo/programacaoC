d89e8ff81582412b3512fc3e8457aa8681d8ec5d
Dyna <denismarc25@outlook.com>
2026-04-02 16:17:50 +0000

    definindo poderes na batalha naval

--- batalha_naval.c
+++ batalha_naval.c
@@ -19,14 +19,35 @@ int main() {
         }
     }
 
-    // definindo navio (simbolizado com 3) em DEF3
-    for(int i = 3; i <= 5; i++) {
-        tabuleiro[2][i] = 3;
+    // definindo poder da cruz
+    for(int i = 0; i <= 4; i++) {
+        tabuleiro[i][3] = 3;
+
+        for(int j = 1; j <= 5; j++) {
+            tabuleiro[2][j] = 3;
+        }
     }
 
-    // definindo navio (simbolizado com 3) em H678
-    for(int i = 5; i <= 7; i++) {
-        tabuleiro[i][7] = 3;
+    // definindo o poder do cone
+    tabuleiro[5][2] = 1;
+    for(int i = 1; i <= 3; i++) {
+        tabuleiro[6][i] = 1;
+
+        for(int j = 0; j <= 4; j++) {
+            tabuleiro[7][j] = 1;
+        }
+    }
+
+    // definindo o poder do octaedro
+    tabuleiro[3][7] = 2;
+    tabuleiro[7][7] = 2;
+    for(int i = 5; i <= 10; i++){
+        tabuleiro[5][i] = 2;
+        
+        for(int j = 6; j <= 8; j++) {
+            tabuleiro[4][j] = 2;
+            tabuleiro[6][j] = 2;
+        }
     }
 
     // imprimindo todos os zeros e substituindo a primeira coluna por números de 1 a 10
